71c76b289d5e42fd7af71cf38c544e1ab2a1fc2e
HariAmbani <ambanihari2004@gmail.com>
2026-01-27 18:17:46 +0530

    Time: 392 ms (20.37%) | Memory: 14.4 MB (99.01%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,48 @@
+import heapq
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
+        """
+        :type times: List[List[int]]
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        if n == 1:
+            return 0
+        
+        adj_list = {}
+
+        for i in range(1,n+1):
+            adj_list[i] = []
+        
+        for src, dst, wt in times:
+            adj_list[src].append([dst, wt])
+        
+        shortest = {}
+        minheap = [[0, k]]
+        time = 0
+
+        while minheap:
+            w1, n1 = heapq.heappop(minheap)
+            if n1 in shortest:
+                continue
+            shortest[n1] = w1
+            if len(shortest) == n:
+                time += w1
+            for n2, w2 in adj_list[n1]:
+                if n2 not in shortest:
+                    heapq.heappush(minheap, [w1+w2, n2])
+        
+        print(shortest)
+        if len(shortest) == n:
+            return time
+        else:
+            return -1
+
+        
+
+
+
+
+
+        
