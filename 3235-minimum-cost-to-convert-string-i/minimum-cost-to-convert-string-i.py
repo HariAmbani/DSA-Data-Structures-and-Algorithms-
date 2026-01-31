a193ec3fc1e8a0cd389f8c428692ca551e54f82a
HariAmbani <ambanihari2004@gmail.com>
2026-01-31 23:58:52 +0530

    Time: 423 ms (99.53%) | Memory: 22.5 MB (69.53%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
@@ -1,44 +1,38 @@
 class Solution(object):
     def minimumCost(self, source, target, original, changed, cost):
-        """
-        :type source: str
-        :type target: str
-        :type original: List[str]
-        :type changed: List[str]
-        :type cost: List[int]
-        :rtype: int
-        """
-        # actually it is graph problem
-        # implementing flowd warshal to find the minimum distance between 2 letters
 
-        #predesecors matrix
-        #pre_mat = [([-1] * 26) for _ in range(26)]
+        INF = 10**18
 
-        #distancc matrix
-        dis_mat = [([float("inf")] * 26) for _ in range(26)]
+        dis = [[INF]*26 for _ in range(26)]
+
+        for i in range(26):
+            dis[i][i] = 0
 
         for i in range(len(original)):
-            #if original[i] != changed[i]
-            dis_mat[ord(original[i])-97][ord(changed[i])-97] = min(dis_mat[ord(original[i])-97][ord(changed[i])-97], cost[i])
-            #pre_mat[ord(original[i])-97][ord(changed[i])-97] = original[i]
-        
-        for i in range(26):
-            dis_mat[i][i] = 0
-        
+            u = ord(original[i]) - 97
+            v = ord(changed[i]) - 97
+            if cost[i] < dis[u][v]:
+                dis[u][v] = cost[i]
 
         for k in range(26):
+            dk = dis[k]
             for i in range(26):
+                dik = dis[i][k]
+                if dik == INF:
+                    continue
+                row_i = dis[i]
                 for j in range(26):
-                    dis_mat[i][j] = min(dis_mat[i][j], dis_mat[i][k]+dis_mat[k][j])
-        
+                    val = dik + dk[j]
+                    if val < row_i[j]:
+                        row_i[j] = val
+
         ans = 0
-        for i in range(len(source)):
-            if source[i] == target[i]:
+        for s, t in zip(source, target):
+            if s == t:
                 continue
-            elif dis_mat[ord(source[i])-97][ord(target[i])-97] == float("inf"):
+            c = dis[ord(s)-97][ord(t)-97]
+            if c == INF:
                 return -1
-            else:
-                ans += dis_mat[ord(source[i])-97][ord(target[i])-97]
+            ans += c
 
         return ans
-    
