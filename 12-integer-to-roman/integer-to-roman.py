07a559f669beaf59ce8f25aa640a605c1e32bb89
HariAmbani <ambanihari2004@gmail.com>
2026-02-02 17:03:57 +0530

    Time: 2 ms (88.50%) | Memory: 12.6 MB (20.19%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -6,12 +6,12 @@ class Solution(object):
         """
         int_to_roman = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"),]
 
-        ans = []
+        ans = ""
         for value, symbol in int_to_roman:
             while num >= value:
-                ans.append(symbol)
+                ans += symbol
                 num -= value
-        return "".join(ans)
+        return ans
 
 
         # my initial solution
