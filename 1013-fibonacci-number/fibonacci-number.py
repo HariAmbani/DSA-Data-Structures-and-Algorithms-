782c03b381d8c8e2a8da36e9b733499c094eeeaa
HariAmbani <ambanihari2004@gmail.com>
2026-02-14 15:46:47 +0530

    Time: 20 ms (49.30%) | Memory: 12.5 MB (54.42%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 1:
+            return n
+        fibo = [-1]*(n+1)
+        fibo[0] = 0
+        fibo[1] = 1
+        for i in range(2,n+1):
+            fibo[i] = fibo[i-1]+fibo[i-2]
+        return fibo[n]
+        
