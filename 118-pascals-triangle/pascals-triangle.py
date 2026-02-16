6b6439e3335f5559fbb050428c2df29f9da17b5b
HariAmbani <ambanihari2004@gmail.com>
2026-02-16 17:39:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.42%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows==1:
+            return [[1]]
+        l = [[1], [1,1]]
+        for i in range(2,numRows):
+            s = [1,]
+            for j in range(1,len(l[i-1])):
+                s.append(l[i-1][j-1] + l[i-1][j])
+            s.append(1)
+            l.append(s)
+        return l
+
+        
