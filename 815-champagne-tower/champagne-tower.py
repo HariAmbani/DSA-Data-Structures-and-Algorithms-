05256f2936e3b7c037b88fcbb6aec71b577caf6e
HariAmbani <ambanihari2004@gmail.com>
2026-02-17 15:31:43 +0530

    Time: 54 ms (86.77%) | Memory: 12.4 MB (97.02%) - LeetSync

--- 815-champagne-tower/champagne-tower.py
+++ 815-champagne-tower/champagne-tower.py
@@ -1,15 +1,15 @@
+from collections import deque
+
 class Solution(object):
     def champagneTower(self, poured, query_row, query_glass):
+        prev_row = [poured] #initial
 
-        dp = [[0.0] * (query_row + 2) for _ in range(query_row + 2)]
-        dp[0][0] = poured
-
-        for r in range(query_row):
-            for c in range(r + 1):
-                if dp[r][c] > 1:
-                    overflow = (dp[r][c] - 1) / 2.0
-                    dp[r+1][c] += overflow
-                    dp[r+1][c+1] += overflow
-                    dp[r][c] = 1
-
-        return min(1, dp[query_row][query_glass])
+        for row in range(1, query_row+1):
+            cur_row = [0.0]*(row+1)
+            for i in range(row):
+                extra = prev_row[i]-1
+                if extra > 0.0:
+                    cur_row[i] += extra * 0.5
+                    cur_row[i+1] += extra * 0.5
+            prev_row = cur_row
+        return min(1, prev_row[query_glass])
