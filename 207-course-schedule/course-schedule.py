130a1bfacdf3ba7a2f35ced9c16bd507af3f625a
HariAmbani <ambanihari2004@gmail.com>
2026-02-21 00:12:17 +0530

    Time: 18 ms (10.92%) | Memory: 16.2 MB (26.65%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -5,60 +5,33 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        if not prerequisites:
-            return True
-        
-        to_see = []
+        preMap = { i:[] for i in range(numCourses)}
 
-        for i in range(numCourses):
-            to_see.append(i)
-        
-        path_map_to = {}
-        path_map_from = {}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
 
-        for i,j in prerequisites:
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if preMap[crs] == []:
+                return True
             
-            if i in path_map_to:
-                if j in path_map_to[i]:
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
                     return False
             
-            if i in path_map_from:
-                path_map_from[i] += 1
-            else:
-                path_map_from[i] = 1
-            
-            if j in path_map_to:
-                path_map_to[j].append(i)
-            else:
-                path_map_to[j] = [i]
-        
-        stack = []
-        seen = 0
-
-        for i in to_see:
-            if i not in path_map_from:
-                stack.append(i)
-                seen += 1
-        
-        while stack:
-            cur = []
-            for i in stack:
-                print("i : ",i)
-                for j in path_map_to.get(i, []):
-                    print("j : ",j)
-                    if path_map_from[j] == 1:
-                        cur.append(j)
-                        seen += 1
-                        del path_map_from[j]
-                    else:
-                        path_map_from[j] -= 1
-                stack = cur
-                
-        if seen == numCourses:
+            visitSet.remove(crs)
+            preMap[crs] = []
             return True
-        else:
-            return False
         
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        
+        return True
 
 
             
