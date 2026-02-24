7b0df9fea884e98387e24f326deb60b62502a600
HariAmbani <ambanihari2004@gmail.com>
2026-02-24 15:22:20 +0530

    Time: 28 ms (73.33%) | Memory: 12.7 MB (66.67%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.py
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.py
@@ -4,19 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-
-        if n == 1:
-            fibo_two = 2
-
+        MOD = 10**9 + 7
         fibo_one = 1
         fibo_two = 2
 
         for i in range(2, n+1):
             temp = fibo_two
-            fibo_two = fibo_one + fibo_two
+            fibo_two = (fibo_one + fibo_two)%MOD
             fibo_one = temp
         
-        ans = pow(fibo_two, 2)%(pow(10, 9)+7)
+        ans = pow(fibo_two, 2)%MOD
 
         return ans
     
