a0b3f13138429e7cbfb4d58b035ca2d618c5c53e
HariAmbani <ambanihari2004@gmail.com>
2026-02-26 22:39:02 +0530

    Time: 1396 ms (26.54%) | Memory: 16.1 MB (13.58%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,22 +1,59 @@
-import bisect
-
 class Solution(object):
     def numMatchingSubseq(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: int
+        """
         dic_words = {}
-        
-        for i, ch in enumerate(s):
-            dic_words.setdefault(ch, []).append(i)
 
+        for i in range(len(s)):
+            if s[i] in dic_words:
+                dic_words[s[i]].append(i)
+            else:
+                dic_words[s[i]] = [i]
+
+        def find_next_greater_index_using_binary_search(i, start, cur):
+            arr = dic_words[i]
+            end = len(arr) - 1
+            ans = -1
+
+            while start <= end:
+                mid = start + (end - start) // 2
+
+                if arr[mid] > cur:
+                    ans = mid
+                    end = mid - 1
+                else:
+                    start = mid + 1
+
+            return ans
+        
         def helper(w):
             cur = -1
-            for ch in w:
-                if ch not in dic_words:
+            dic_letter = {}
+            for i in w:
+                if i not in dic_words:
                     return False
-                idx_list = dic_words[ch]
-                pos = bisect.bisect_right(idx_list, cur)
-                if pos == len(idx_list):
-                    return False
-                cur = idx_list[pos]
+                elif i not in dic_letter:
+                    j = find_next_greater_index_using_binary_search(i, 0, cur)
+                    if j == -1:
+                        return False
+                    else:
+                        dic_letter[i] = j
+                        cur = dic_words[i][j]
+                else:
+                    j =find_next_greater_index_using_binary_search(i, dic_letter[i]+1, cur)
+                    if j == -1:
+                        return False
+                    else:
+                        dic_letter[i]= j
+                        cur = dic_words[i][j]
             return True
 
-        return sum(helper(w) for w in words)
+        ans = 0
+        for j in words:
+            if helper(j):
+                ans += 1
+        return ans               
+        
