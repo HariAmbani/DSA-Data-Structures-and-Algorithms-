357669eb3640335798f48621357257dee926101b
HariAmbani <ambanihari2004@gmail.com>
2026-03-12 23:05:50 +0530

    Time: 9 ms (65.27%) | Memory: 12.7 MB (17.51%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -1,39 +1,22 @@
 class Solution(object):
     def numSpecial(self, mat):
-        """
-        :type mat: List[List[int]]
-        :rtype: int
-        """
-        ans = 0
-
         rows = len(mat)
         cols = len(mat[0])
 
-        indexes_not_to_check = set()
+        row_count = [0]*rows
+        col_count = [0]*cols
 
         for i in range(rows):
             for j in range(cols):
-                if mat[i][j] == 1 and (i,j) not in indexes_not_to_check:
-                    a_found = 0
-                    b_found = 0
-                    for a in range(rows):
-                        if a == i:
-                            continue
-                        if mat[a][j] == 1:
-                            a_found = 1
-                            indexes_not_to_check.add((a,j))
-                            break
-                    if a_found == 0:
-                        for b in range(cols):
-                            if b == j:
-                                continue
-                            if mat[i][b] == 1:
-                                b_found = 1
-                                indexes_not_to_check.add((i,b))
-                                break
-                        if b_found == 0:
-                            ans += 1
-        
-        return ans
+                if mat[i][j] == 1:
+                    row_count[i] += 1
+                    col_count[j] += 1
 
-        
+        ans = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if mat[i][j] == 1 and row_count[i] == 1 and col_count[j] == 1:
+                    ans += 1
+
+        return ans
