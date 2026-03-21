f69451902d284ec05a27ad66d3d5493a81c175d7
HariAmbani <ambanihari2004@gmail.com>
2026-03-22 01:04:43 +0530

    Time: 1 ms (48.15%) | Memory: 12.7 MB (51.85%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.py
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.py
@@ -8,13 +8,6 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         for i in range(k//2):
-            print("x : ",x)
-            print("y : ",y)
-            print("k : ",k)
-            print("i : ",i)
-            print("grid[x][y:y+k] : ", grid[x][y:y+k])
-            print("grid[x+k-i-1][y:y+k] : ", grid[x+k-i-1][y:y+k])
-            print("---------------")
             temp = grid[x+i][y:y+k]
             grid[x+i][y:y+k] = grid[x+k-i-1][y:y+k]
             grid[x+k-i-1][y:y+k] = temp
