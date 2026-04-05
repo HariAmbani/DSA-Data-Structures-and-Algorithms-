4dfddfeec180ec503fb6d88b683892b320ffe578
HariAmbani <ambanihari2004@gmail.com>
2026-04-05 16:11:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (34.64%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -4,20 +4,4 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        vertical = 0
-        horizontal = 0
-
-        for i in moves:
-            if i == 'U':
-                vertical += 1
-            elif i == 'D':
-                vertical -= 1
-            elif i == "L":
-                horizontal += 1
-            elif i == "R":
-                horizontal -= 1
-        
-        if ((vertical == 0) and (horizontal == 0)):
-            return True
-        else:
-            return False
+        return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
