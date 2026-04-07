f129b2616ee68071eb72e97f39034116ed5c41d4
HariAmbani <ambanihari2004@gmail.com>
2026-04-08 00:47:03 +0530

    Time: 53 ms (56.76%) | Memory: 17.1 MB (45.95%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -8,28 +8,30 @@ class Solution(object):
         #             north   east   south   west
         directions = ((0,1), (1,0), (0,-1), (-1,0))
         cur_dir = 0
-        cur_pos = [0,0]
+        x, y = 0, 0
+        xd, yd = directions[0][0], directions[0][1]
 
-        obstacles_set = set()
-        for i,j in obstacles:
-            obstacles_set.add((i,j))
+        obstacles_set = set(map(tuple, obstacles))
 
         further_point = 0
 
         for i in commands:
             if i == -1:
                 cur_dir = (cur_dir+1)%4
+                xd = directions[cur_dir][0]
+                yd = directions[cur_dir][1]
             elif i == -2:
                 cur_dir = (cur_dir-1)%4
+                xd = directions[cur_dir][0]
+                yd = directions[cur_dir][1]
             else:
                 for j in range(i):
-                    new_x = cur_pos[0]+directions[cur_dir][0]
-                    new_y = cur_pos[1]+directions[cur_dir][1]
+                    new_x = x+xd
+                    new_y = y+yd
                     if (new_x, new_y) not in obstacles_set:
-                        cur_pos[0], cur_pos[1] = new_x, new_y
-                        new_length = (cur_pos[0]*cur_pos[0]) + (cur_pos[1]*cur_pos[1])
-                        if new_length > further_point:
-                            further_point = new_length
+                        x, y = new_x, new_y
+                        new_length = (x*x) + (y*y)
+                        further_point = max(further_point, new_length)
                     else:
                         break
             
