705877c8be6131f00fe4d5e29d9db719fbedd485
HariAmbani <ambanihari2004@gmail.com>
2026-04-16 15:37:34 +0530

    Time: 545 ms (45.73%) | Memory: 40.7 MB (76.84%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxi = float('inf')
+
+        num_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in num_dict:
+                num_dict[nums[i]].append(i)
+            else:
+                num_dict[nums[i]]=[i]
+        
+        for j in num_dict.values():
+            if len(j) > 2:
+                for k in range(len(j)-2):
+                    maxi = min(maxi, (j[k+1]-j[k])+(j[k+2]-j[k+1])+(j[k+2]-j[k]))
+        
+        if maxi == float('inf'):
+            return -1
+        else:
+            return maxi
+        
