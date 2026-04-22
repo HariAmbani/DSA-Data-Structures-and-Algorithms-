2b02dab94ad724ac35da467b881c43198c9ad6a7
HariAmbani <ambanihari2004@gmail.com>
2026-04-22 17:11:44 +0530

    Time: 479 ms (100.00%) | Memory: 65 MB (50.00%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.py
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.py
@@ -3,45 +3,63 @@ from collections import defaultdict, deque, Counter
 class Solution(object):
     def minimumHammingDistance(self, source, target, allowedSwaps):
         n = len(source)
-        
-        # Step 1: Build graph
+        ans = 0        
+
+        if len(allowedSwaps) == 0:
+            for i in range(n):
+                if source[i] != target[i]:
+                    ans += 1
+            
+            return ans
+
+
         graph = defaultdict(list)
         for i, j in allowedSwaps:
             graph[i].append(j)
             graph[j].append(i)
         
+        to_visit = []
         visited = set()
-        ans = 0
+        connected = []
         
-        # Step 2: Traverse each connected component
+        for key in graph.keys():
+            if key not in visited:
+                cur = []
+                q = deque([key])
+                while q:
+                    now = q.popleft()
+                    if now not in visited:
+                        cur.append(now)
+                        visited.add(now)
+                    for val in graph[now]:
+                        if val not in visited:
+                            q.append(val)
+                connected.append(cur)
+
+        print(connected)
+
+        for values in connected:
+            count = Counter()
+
+            for val in values:
+                count[source[val]] += 1
+
+            for val in values:
+                if count[target[val]] > 0:
+                    count[target[val]] -= 1
+                else:
+                    ans += 1
+
+
         for i in range(n):
-            if i not in visited:
-                queue = deque([i])
-                component = []
-                
-                while queue:
-                    node = queue.popleft()
-                    
-                    if node in visited:
-                        continue
-                    
-                    visited.add(node)
-                    component.append(node)
-                    
-                    for nei in graph[node]:
-                        if nei not in visited:
-                            queue.append(nei)
-                
-                # Step 3: Count values in this component
-                count = Counter()
-                for idx in component:
-                    count[source[idx]] += 1
-                
-                # Step 4: Try matching target
-                for idx in component:
-                    if count[target[idx]] > 0:
-                        count[target[idx]] -= 1
-                    else:
-                        ans += 1
+            if i not in visited and source[i] != target[i]:
+                ans += 1
         
+        return ans
+            
+
+
+        
+        
+                
         return ans
