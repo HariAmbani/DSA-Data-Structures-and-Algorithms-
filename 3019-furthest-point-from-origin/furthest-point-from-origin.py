014b630b3cba6e711ebcf54298a4924f891ed191
HariAmbani <ambanihari2004@gmail.com>
2026-04-24 12:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.41%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.py
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.py
@@ -4,21 +4,26 @@ class Solution(object):
         :type moves: str
         :rtype: int
         """
-        cur = 0
-        no_ = 0
+        l = moves.count("L")
+        r = moves.count("R")
+        u = moves.count("_")
+        return (abs(l-r)+u) 
+
+        # cur = 0
+        # no_ = 0
         
-        for i in moves:
-            if i == "_":
-                no_ += 1
-            elif i == "L":
-                cur -= 1
-            elif i == "R":
-                cur += 1
+        # for i in moves:
+        #     if i == "_":
+        #         no_ += 1
+        #     elif i == "L":
+        #         cur -= 1
+        #     elif i == "R":
+        #         cur += 1
         
-        if cur >= 0:
-            return cur+no_
-        else:
-            return abs(cur-no_)
+        # if cur >= 0:
+        #     return cur+no_
+        # else:
+        #     return abs(cur-no_)
         
 
 
