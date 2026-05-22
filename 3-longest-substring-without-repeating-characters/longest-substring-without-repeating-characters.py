53540ea9c7ddb78795e261bca18ab94c5ac0e61c
HariAmbani <ambanihari2004@gmail.com>
2026-05-22 16:17:56 +0530

    Time: 15 ms (91.91%) | Memory: 13.3 MB (21.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,11 +3,13 @@ class Solution(object):
         char_dict = {}
         start = 0
         max_len = 0
+
         for i in range(len(s)):
             if s[i] in char_dict and char_dict[s[i]] >= start:
-                start = char_dict[s[i]]+1
+                start = char_dict[s[i]] + 1
             max_len = max(max_len, i-start+1)
             char_dict[s[i]] = i
+
         return max_len 
 
 
