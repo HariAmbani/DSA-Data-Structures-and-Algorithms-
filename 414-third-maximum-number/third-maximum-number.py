4ef430859ddce6c478a12f97212f23194aeb8672
HariAmbani <ambanihari2004@gmail.com>
2026-05-24 12:20:53 +0530

    Time: 3 ms (33.10%) | Memory: 13 MB (43.21%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -4,28 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxi1 = float('-inf')
-        maxi2 = float('-inf')
-        maxi3 = float('-inf')
+        nums = list(set(nums))
 
-        for i in nums:
-            if i > maxi1:
-                print('1')
-                maxi3 = maxi2
-                maxi2 = maxi1
-                maxi1 = i
-            elif i > maxi2 and i != maxi1:
-                print('2')
-                maxi3 = maxi2
-                maxi2 = i
-            elif i > maxi3 and i not in [maxi1, maxi2]:
-                print('3')
-                maxi3 = i
+        nums.sort(reverse=True)
 
-        print("-------------")
-        print(maxi1)
-        print(maxi2)
-        print(maxi3)
-
-        return maxi3 if maxi3 != float('-inf') else maxi1
+        if len(nums) > 2:
+            return nums[2]
+        
+        return nums[0]
         
