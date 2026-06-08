1f049d6a7d533167fac7a2ca07f590983ba09158
HariAmbani <ambanihari2004@gmail.com>
2026-06-08 21:43:45 +0530

    Time: 55 ms (84.65%) | Memory: 29.6 MB (42.54%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -5,36 +5,17 @@ class Solution(object):
         :type pivot: int
         :rtype: List[int]
         """
-        n = len(nums)
-        new = [0]*n
-        
-        less = 0
-        more = 0
+        less = []
+        more = []
         same = 0
 
         for i in nums:
             if i < pivot:
-                less += 1
+                less.append(i)
             elif i > pivot:
-                more += 1
+                more.append(i)
             else:
                 same += 1
         
-        less_pos = 0
-        same_pos = less
-        more_pos = less + same
-
-        for i in nums:
-            if i < pivot:
-                new[less_pos] = i
-                less_pos += 1
-            elif i > pivot:
-                new[more_pos] = i
-                more_pos += 1
-            else:
-                new[same_pos] = i
-                same_pos += 1
-    
-        nums = new
-        return nums
+        return less+[pivot]*same+more
                 
