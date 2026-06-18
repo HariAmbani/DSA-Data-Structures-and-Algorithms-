ba97487a612e406cbd7b479496b132ac51aaa2b6
HariAmbani <ambanihari2004@gmail.com>
2026-06-18 22:14:43 +0530

    Time: 47 ms (90.68%) | Memory: 31.3 MB (67.66%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 class Solution(object):
     def checkSubarraySum(self, nums, k):
         """
@@ -6,38 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        if len(nums) == 1:
-            return False 
-            
-        sum_dict = defaultdict(int)
-        sum_dict[0] = 0
 
-        cur_sum = 0
-        zero_count = []
-
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zero_count.append(i)
-                continue
-            cur_sum = (cur_sum + nums[i])%k
-            if cur_sum in sum_dict and (i-sum_dict[cur_sum]) >= 1 and (nums[i] % k != 0 or ((nums[i]+nums[i-1]) % k == 0)) :
-                return True
-            if cur_sum not in sum_dict:
-                sum_dict[cur_sum] = i
-        
-        for i in zero_count:
-            if i == 0:
-                if nums[i+1]%k == 0:
-                    return True
-            elif i == len(nums)-1:
-                if nums[i-1]%k == 0:
+        remainders = {0: -1}
+        prefix_sum = 0
+    
+        for i, num in enumerate(nums):
+            prefix_sum += num
+            remainder = prefix_sum % k
+            if remainder in remainders:
+                if i - remainders[remainder] > 1:
                     return True
             else:
-                if nums[i-1]%k == 0 or nums[i+1]%k == 0:
-                    return True
-        
-        return False
-
-        
-
-        
+                remainders[remainder] = i
+        return False        
