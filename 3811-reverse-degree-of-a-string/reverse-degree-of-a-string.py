1bdcc1f54c73f654f7dafd14392dfb010a29820f
HariAmbani <ambanihari2004@gmail.com>
2026-09-21 17:02:48 +0530

    Time: 12 ms (41.54%) | Memory: 12.5 MB (23.08%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -7,7 +7,7 @@ class Solution(object):
         ans = 0
 
         for i in range(len(s)): 
-            ans += (i+1)*(ord('z')-ord(s[i])+1)
+            ans += (i+1)*(123-ord(s[i]))
         
         return ans
         
